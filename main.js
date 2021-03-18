c36acdc17651696479bede50732124a32f0a5198
AlexaDob <58271470+AlexaDob@users.noreply.github.com>
2021-03-18 15:24:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -37,3 +37,87 @@ function new_image(get_image){
         canvas.add(block_img_object);
     });
 }
+
+window.addEventListener("keydown", my_keydown);
+ 
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+    if(e.shiftKey == true && keyPressed == '80'){
+    console.log("p and shift pressed together");
+    block_image_width = block_image_width + 10;
+    block_image_height = block_image_height + 10;
+    document.getElementById("current_width").innerHTML = block_image_width;
+    document.getElementById("current_height").innerHTML = block_image_height;   
+    }
+
+    if(e.shiftKey == true && keyPressed == '77'){
+    console.log("m and shift pressed together");
+    block_image_width = block_image_width - 10;
+    block_image_height = block_image_height - 10;
+    document.getElementById("current_width").innerHTML = block_image_width;
+    document.getElementById("current_height").innerHTML = block_image_height;
+    }
+
+//Face Keys
+    if(keyPressed == '70'){
+        new_image('ironman_face.png');
+        console.log("f key");
+    }
+
+    if(e.shiftKey == true && keyPressed == '70'){
+        new_image('thor_face.png');
+        console.log("f key and Shift");
+    }
+
+//Body Keys
+    if(keyPressed == '66'){
+        new_image('ironman_body.png');
+        console.log("b key");
+    }
+
+    if(e.shiftKey == true && keyPressed == '66'){
+        new_image('hulkd_body.png');
+        console.log("b key and Shift");
+    }
+
+//Leg Keys
+    if(keyPressed == '76'){
+        new_image('ironman_legs.png');
+        console.log("l key");
+    }
+
+    if(e.shiftKey == true && keyPressed == '66'){
+        new_image('spiderman_legs.png');
+        console.log("l key and Shift");
+    }
+
+//Left Arm Keys
+    if(keyPressed == '72'){
+        new_image('captain_america_left_hand.png');
+        console.log("h key");
+    }
+
+    if(e.shiftKey == true && keyPressed == '72'){
+        new_image('thor_right_hand.png');
+        console.log("h key and Shift");
+    }
+
+//Right Arm Keys
+    if(keyPressed == '82'){
+        new_image('thor_right_hand.png');
+        console.log("r key");
+    }
+
+    if(e.shiftKey == true && keyPressed == '82'){
+        new_image('spiderman_right_hand.png');
+        console.log("r key and Shift");
+    }
+
+// Arrow Keys
+    if(keyPressed == '38'){
+        up();
+        console.log("up key");
+    }
+}
